e01e2aae7eac73d2f29963146857c0842b204408
Dimas Oktanugraha <d.oktanugraha@gmail.com>
2020-04-13 07:42:10 +0700

    rv 2

--- app/src/main/java/com/modtion/fightcovid/adapter/WebAdapter.kt
+++ app/src/main/java/com/modtion/fightcovid/adapter/WebAdapter.kt
@@ -3,6 +3,7 @@ package com.modtion.fightcovid.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.modtion.fightcovid.R
 import com.modtion.fightcovid.model.WebModel
@@ -18,9 +19,13 @@ class WebAdapter(private val items: List<WebModel>, private val listener: (WebMo
     }
 
     class WebViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val item_tv_city: TextView = itemView.findViewById(R.id.item_tv_city)
+        val item_tv_web: TextView = itemView.findViewById(R.id.item_tv_web)
+
         fun bindItem(items: WebModel, listener: (WebModel) -> Unit) {
-            item_city.text = items.city
-            item_web.text = items.website
+            item_tv_city.text = items.city
+            item_tv_web.text = items.website
             itemView.setOnClickListener {
                 listener(items)
             }
--- app/src/main/java/com/modtion/fightcovid/fragment/WebsiteFragment.kt
+++ app/src/main/java/com/modtion/fightcovid/fragment/WebsiteFragment.kt
@@ -5,9 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.modtion.fightcovid.R
+import com.modtion.fightcovid.adapter.WebAdapter
+import com.modtion.fightcovid.model.WebModel
+import kotlinx.android.synthetic.main.fragment_web.*
 
 class WebsiteFragment: Fragment() {
+
+    private var provinceList: MutableList<WebModel> = mutableListOf()
+    private var cityList: MutableList<WebModel> = mutableListOf()
 
     companion object{
         fun newInstance() : MapFragment{
@@ -23,5 +30,39 @@ class WebsiteFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initProvince()
+    }
+
+    private fun initProvince() {
+        val province = resources.getStringArray(R.array.web_province_name)
+        val provWeb = resources.getStringArray(R.array.web_province)
+        provinceList.clear()
+        for (i in province.indices) {
+            provinceList.add(WebModel(province[i], provWeb[i]))
+        }
+
+        rv_province.layoutManager = LinearLayoutManager(activity)
+        rv_province.setHasFixedSize(true)
+        rv_province.adapter = WebAdapter(provinceList){
+
+        }
+
+        intCity()
+    }
+
+    private fun intCity() {
+        val city = resources.getStringArray(R.array.web_city_name)
+        val cityWeb = resources.getStringArray(R.array.web_city)
+        cityList.clear()
+        for (i in city.indices) {
+            provinceList.add(WebModel(city[i], cityWeb[i]))
+        }
+
+        rv_district.layoutManager = LinearLayoutManager(activity)
+        rv_district.setHasFixedSize(true)
+        rv_district.adapter = WebAdapter(provinceList){
+
+        }
     }
 }
