ae6fce38194adb804a272b2e2b525e1fc5805ccb
Dimas Oktanugraha <d.oktanugraha@gmail.com>
2020-04-13 07:01:49 +0700

    data web

---
+++ app/src/main/java/com/modtion/fightcovid/activity/WebsiteActivity.kt
@@ -0,0 +1,13 @@
+package com.modtion.fightcovid.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.modtion.fightcovid.R
+
+class WebsiteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_website)
+    }
+}
